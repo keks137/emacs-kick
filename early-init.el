ace3a61e1ef92303d5faef5206ab22d425dc4cd7
keks137 <guntzjonas@gmail.com>
2025-06-17 18:40:49 +0200

    idk

---
+++ early-init.el
@@ -0,0 +1,4 @@
+;; Set the initial frame parameters to avoid a flash of a light background.
+(setq default-frame-alist
+'((background-color . "#1e1e1e")   ;; dark background color
+(foreground-color . "#000000")))   ;; light text
